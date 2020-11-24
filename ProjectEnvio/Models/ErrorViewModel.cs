368f6584e0b0e16eee45d3be434d91c8db0f94f6
Brian velasquez <Brianvelasquez@sqdm.com>
2020-11-23 22:26:00 -0500

    Add project files.

---
+++ ProjectEnvio/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjectEnvio.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
